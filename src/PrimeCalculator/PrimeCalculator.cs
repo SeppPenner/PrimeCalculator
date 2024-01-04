78b9cfa77034313add8ae8971e54c2d515e031ee
SeppPenner <HansM2013@gmx.de>
2024-01-04 09:11:38 +0100

    Smaller fix.

--- src/PrimeCalculator/PrimeCalculator.cs
+++ src/PrimeCalculator/PrimeCalculator.cs
@@ -85,7 +85,7 @@ public partial class PrimeCalculator : Form
     /// <param name="e">The event args.</param>
     private void ComboBoxLanguageSelectedIndexChanged(object sender, EventArgs e)
     {
-        var selectedItem = this.comboBoxLanguage.SelectedItem.ToString();
+        var selectedItem = this.comboBoxLanguage.SelectedItem?.ToString();
 
         if (string.IsNullOrWhiteSpace(selectedItem))
         {
